20bb2b839afc3799adb8e7a5294b7ba6fdaba144
harvanchik <66141166+harvanchik@users.noreply.github.com>
2023-11-29 00:20:23 -0800

    ran rn fix up

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+import 'react-native-gesture-handler';
 import Game from './src/components/Game';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
-const App = () => <Game />;
+const App = () => (
+  <GestureHandlerRootView style={{flex: 1}}>
+    <Game />
+  </GestureHandlerRootView>
+);
 
 export default App;
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import {Colors} from '../styles/colors';
+import {PanGestureHandler} from 'react-native-gesture-handler';
 
 export default function Game(): JSX.Element {
-  return <SafeAreaView style={styles.container} />;
+  const handleGesture = (event: any) => {
+    console.log(event.nativeEvent);
+  };
+
+  return (
+    <PanGestureHandler onGestureEvent={handleGesture}>
+      <SafeAreaView style={styles.container} />
+    </PanGestureHandler>
+  );
 }
 
 const styles = StyleSheet.create({
